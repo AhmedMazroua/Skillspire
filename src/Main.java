a436756b106d54310a750b66d06a0d74a383b0ad
AhmedMazroua <126991045+AhmedMazroua@users.noreply.github.com>
2024-09-03 11:09:39 -0700

    4th  commit

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,7 @@ To submit, send a link to the repository in GitHub. Try to use branching and mer
  */
 
 // imported Random object module
-
+import java.util.Random;
 // Defined main class
 public class Main {
     // Defined Main method
@@ -56,20 +56,50 @@ public class Main {
         calculateFloodRisk();
         totalCalculatedRisk();
     }
-
+    // Defined calculatedCrimeRisk method
     public static int calculateCrimeRisk(){
-
+        // create Random object
+        Random random = new Random();
+        // Generate random number from 0 to 100
+        int num = random.nextInt(101);
+        // Return result
+        return  num;
     }
 
+    // Defined calculatedEarthquakeRisk method
     public static int calculateEarthquakeRisk(){
+        // create Random object
+        Random random = new Random();
+        // Generate random number from 0 to 100
+        int num = random.nextInt(101);
+        // Return result
+        return  num;
 
     }
-
+    // Defined calculatedFloodRisk method
     public static int calculateFloodRisk(){
-
+        // Create Random object
+        Random random = new Random();
+        // Generate random number from 0 to 100
+        int num = random.nextInt(101);
+        // Return result
+        return  num;
     }
 
+    // Defined totalCalculatedRisk method
     public static int totalCalculatedRisk(){
+        // Initialized variable to store methods
+        int a = calculateCrimeRisk();
+        int b = calculateEarthquakeRisk();
+        int c = calculateFloodRisk();
 
+        // Sum each returned values into 1 variable
+        int result = a + b + c;
+
+        // Print to console result
+        System.out.println("The total calculated risk level is" + " " + result);
+        // Return result
+        return result;
     }
+
 }
